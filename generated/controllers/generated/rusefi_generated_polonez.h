582bb2cf00483e9adada32c9c86d84672722b6ef
GitHub git update Action <action@github.com>
2026-05-19 00:38:29 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_polonez.h
+++ generated/controllers/generated/rusefi_generated_polonez.h
@@ -2181,7 +2181,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.05.18.polonez.1418248138"
+#define TS_SIGNATURE "rusEFI main.2026.05.19.polonez.1418248138"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
