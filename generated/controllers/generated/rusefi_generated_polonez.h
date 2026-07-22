e9271fa764f8d0f4d6d74c0d2f894219c7aa720d
GitHub git update Action <action@github.com>
2026-07-22 00:28:29 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_polonez.h
+++ generated/controllers/generated/rusefi_generated_polonez.h
@@ -1626,7 +1626,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1805270963
+#define SIGNATURE_HASH 2373737581
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2264,7 +2264,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.07.21.polonez.1805270963"
+#define TS_SIGNATURE "rusEFI main.2026.07.22.polonez.2373737581"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_polonez.h
+++ generated/controllers/generated/signature_polonez.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1805270963
-#define TS_SIGNATURE "rusEFI main.2026.07.21.polonez.1805270963"
+#define SIGNATURE_HASH 2373737581
+#define TS_SIGNATURE "rusEFI main.2026.07.22.polonez.2373737581"
