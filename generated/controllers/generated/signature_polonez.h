d4b23f9889b111ad71e6ee9debfb534bfed680b7
GitHub git update Action <action@github.com>
2026-08-03 00:31:45 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_polonez.h
+++ generated/controllers/generated/signature_polonez.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2908112193
-#define TS_SIGNATURE "rusEFI main.2026.08.02.polonez.2908112193"
+#define SIGNATURE_HASH 1800429574
+#define TS_SIGNATURE "rusEFI main.2026.08.03.polonez.1800429574"
